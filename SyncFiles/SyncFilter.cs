99e7d03e5072ce5af066f508baac5292468b6e18
mark heath (@somnio) <silicontrip@gmail.com>
2021-09-06 22:30:54 +1000

    SyncFilter, file selection strategy

---
+++ SyncFiles/SyncFilter.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Management.Automation;
+
+namespace net.ninebroadcast
+{
+
+    public class SyncFilter
+	{
+		private string[] includeList;
+
+
+		public bool include(string file, string[] includeList)
+		{
+			bool include = true;
+			if (includeList != null)
+            {
+                include = false;
+                foreach (string m in includeList)
+                {
+					// string, string, method
+					Match me = Regex.Match(file,m);
+					if (me.Success) { include = true; }
+				}
+			}
+		}
+
+
+	}
+}
