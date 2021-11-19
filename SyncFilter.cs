bf50e3840278982e9e1242e58938ceee5fc53101
Mark Heath - ADMIN v2 <cs_mheath@nine.com.au>
2021-11-19 12:06:01 +1100

    Fixing the branches

--- SyncFilter.cs
+++ SyncFilter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Management.Automation;
+using System.Text.RegularExpressions;
 
 namespace net.ninebroadcast
 {
@@ -12,6 +13,9 @@ namespace net.ninebroadcast
 		// update
 		// size
 		// checksum
+
+		// some sort of observer or strategy pattern.
+		// add these to an array and evaluate the array.
 
 		public bool include(string file, string[] includeList)
 		{
@@ -26,8 +30,22 @@ namespace net.ninebroadcast
 					if (me.Success) { include = true; }
 				}
 			}
+			return include;
 		}
 
-
+		public bool exclude (string file, string[] excludeList)
+		{
+			bool include = true;
+			if (excludeList != null)
+			{
+				foreach (string m in excludeList)
+				{
+					Match me = Regex.Match(file,m);
+					if (me.Success) { include = false; }
+					// if (LikeOperator.LikeString(file, m, Microsoft.VisualBasic.CompareMethod.Text)) { include = false; }
+				}
+			}
+			return include;
+		}
 	}
 }
